ccb7984e505ca8d6d26b535d5ae908f095e19a3d
Bruna Sales <144288264+brunasalesdesousa@users.noreply.github.com>
2024-05-18 14:11:33 -0300

    primeira aula da segunda semana PUCPR

---
+++ aulasemana2/src/Retangulo.java
@@ -0,0 +1,35 @@
+public class Retangulo {
+    // atributos
+    float altura;
+    float comprimento;
+
+    // métodos
+    Retangulo(float alturaInicial) {
+        this.altura = alturaInicial;
+        this.comprimento = 10.0f;
+    }
+
+    Retangulo (float alturaInicial, float comprimentoInicial ) {
+        this.altura = alturaInicial;
+        this.comprimento = comprimentoInicial;
+    }
+    float calcularPerimetro() {
+        return (2 * altura) + (2 * comprimento);
+         }
+    float calcularArea() {
+        return altura * comprimento;
+
+    }
+
+    // ponto de partida do nosso código
+    public static void main(String[] args) {
+        System.out.println("Bem-Vindo ao mundo dos retângulos!");
+
+        Retangulo ret1 = new Retangulo(5.2f, 12.5f);
+                System.out.println(ret1.calcularPerimetro());
+
+        Retangulo ret2 = new Retangulo(10.3f);
+               System.out.println(ret2.calcularPerimetro());
+
+    }
+}
